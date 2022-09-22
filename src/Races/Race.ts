53f50f3585b5cea73a467a6001adad330e38ac88
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-09-22 20:04:44 -0300

    fix: adicionado o parâmetro 'new' na implementação do lançamento do erro

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,11 +1,11 @@
 abstract class Race {
-  constructor(readonly name: string = '', readonly dexterity: number = 0) { }
+  constructor(readonly name: string, readonly dexterity: number) { }
 
   static createdRacesInstances(): number {
-    throw Error('Not implemented');
+    throw new Error('Not implemented');
   }
 
-  abstract get maxLifePoints(): number;
+  abstract get maxLifePoints(): number; 
 }
 
 export default Race;
